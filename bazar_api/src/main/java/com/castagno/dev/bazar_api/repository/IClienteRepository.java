181b55279fb1ac0d0d3434ddbafb5f1a03acac8e
LeonelLopez14 <ll.lopezleonel99@gmail.com>
2026-04-10 23:53:12 -0300

    v0.1.3: BD con h.2 en application.properties

--- bazar_api/src/main/java/com/castagno/dev/bazar_api/repository/IClienteRepository.java
+++ bazar_api/src/main/java/com/castagno/dev/bazar_api/repository/IClienteRepository.java
@@ -1,9 +1,9 @@
 package com.castagno.dev.bazar_api.repository;
 
-import com.castagno.dev.bazar_api.dto.ClienteDTO;
+import com.castagno.dev.bazar_api.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IClienteRepository extends JpaRepository<ClienteDTO, Long> {
+public interface IClienteRepository extends JpaRepository<Cliente, Long> {
 }
